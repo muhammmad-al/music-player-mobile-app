f2a0eee57056bf1c045840233f55396cb3e3fe34
honeyloupe <wtk9sa@virginia.edu>
2024-05-26 16:27:23 -0400

    changed the firebase configs to connect to my (Honeyloupe) firebase account

--- app/index.tsx
+++ app/index.tsx
@@ -16,12 +16,13 @@ import { initializeApp } from 'firebase/app';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA8hr1oOEwu4w4bMQwRTiQjNNwHJzZktkY",
-  authDomain: "soundrevive-91d60.firebaseapp.com",
-  projectId: "soundrevive-91d60",
-  storageBucket: "soundrevive-91d60.appspot.com",
-  messagingSenderId: "341252652672",
-  appId: "1:341252652672:web:9f3c574e1ca02a59f97065"
+  apiKey: "AIzaSyB4RR8z55G29L0hweKx72P2dsE07e04CTg",
+  authDomain: "musicdistribution-fea87.firebaseapp.com",
+  projectId: "musicdistribution-fea87",
+  storageBucket: "musicdistribution-fea87.appspot.com",
+  messagingSenderId: "286063233697",
+  appId: "1:286063233697:web:22da5691adaf320142fee0",
+  measurementId: "G-0R3BBY9TJ6"
 };
 
 // Initialize Firebase
